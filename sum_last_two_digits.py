95dd5f6bc6bc7f245b054e7dc59797498ca40752
Vladyslav Synytskyi <vladyslavsynytskyi1019@gmail.com>
2025-05-23 14:16:02 +0300

    Create sum_last_two_digits.py
    ## sum_last_two_digits.py
    
    This script sums the last two digits of two user-provided numbers and outputs the result.

---
+++ sum_last_two_digits.py
@@ -0,0 +1,7 @@
+data_1 = input('Enter the first number: ')
+data_2 = input('Enter the second number: ')
+
+last_two_1 = int(data_1) % 100
+last_two_2 = int(data_2) % 100
+
+print(last_two_1 + last_two_2)
