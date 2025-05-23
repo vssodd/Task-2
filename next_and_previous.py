76eff49d04d58af6f53d46941e7b30c3d415cf32
Vladyslav Synytskyi <vladyslavsynytskyi1019@gmail.com>
2025-05-23 14:13:12 +0300

    Create next_and_previous.py
    ## next_and_previous.py
    
    This simple Python script reads a number from the user and outputs the number that comes before it and after it.

---
+++ next_and_previous.py
@@ -0,0 +1,7 @@
+data_1 = input('Enter a number: ')
+
+number = int(data_1)  # Convert input to integer
+
+print('The number after', number, 'is', number + 1)
+
+print('The number before', number, 'is', number - 1)
